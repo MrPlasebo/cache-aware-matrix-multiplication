01c97bd0e491571509c982cac95c14e5ec9af243
Mirsad Özkan <140523748+MrPlasebo@users.noreply.github.com>
2026-05-06 08:54:34 +0300

    Add files via upload

---
+++ src/optimized_matrix.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef N
+#define N 500
+#endif
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+void initMatrices() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % 10;
+            B[i][j] = rand() % 10;
+            C[i][j] = 0;
+        }
+    }
+}
+
+int main() {
+    srand(0);
+
+    initMatrices();
+
+    printf("Optimized matrix multiplication\n");
+
+    clock_t start = clock();
+
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    clock_t end = clock();
+
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("C[0][0] = %d\n", C[0][0]);
+    printf("Time: %f seconds\n", time_spent);
+
+    return 0;
+}
